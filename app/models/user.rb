33fa2534a85b9963558a359641e4f0d77ec406da
elenamvk <elena.sanna.mvk@gmail.com>
2015-05-27 14:55:25 +0100

    devise and roles existing... time to can can can

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :requests, dependent: :destroy
   has_many :offers, dependent: :destroy
+
+  # checks to see if you have chosen one of given possibilities
+    validates :role, inclusion: { in: %w(band host admin),
+      message: "you have not chosen a valid role" }, presence: true
 
   
 end
